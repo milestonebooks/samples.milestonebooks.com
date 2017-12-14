3a9b2e6947b630931183cdf83cefac5ecb27ef41
milestonebooks <office@milestonebooks.com>
2017-12-14 15:01:24 -0800

    moved player to component

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,20 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
         samples
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
       <div class="links">
-        <a href="/20-HEBCD/">Item</a>
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="/ITEM/">Test Item Page</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
@@ -43,17 +35,5 @@ export default {
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
